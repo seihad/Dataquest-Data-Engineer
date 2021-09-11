c7787e99ac307a1cf391b712ddc1d66fb8257a23
seihad <seil.hadiward@gmail.com>
2021-09-11 23:38:11 +0200

    update

---
+++ Step 5 - Handling Large Data Sets in Python/1. Numpy for Data Engineers/3_broadcasting_numpy_arrays.py
@@ -0,0 +1,17 @@
+'''
+1.Introduction
+'''
+# import numpy as np
+# x = np.array([
+#  [7., 9., 2., 2.],
+#  [3., 2., 6., 4.],
+#  [5., 6., 5., 7.]
+# ])
+# ones = np.ones((3,4))
+# print(ones)
+# x = x - ones
+# print(x)
+
+'''
+2.Broadcasting With a Single Value
+'''
