500f1407a52aac75e824db16b967aba93922f108
seihad <seil.hadiward@gmail.com>
2021-09-12 18:06:17 +0200

    step5 mod1 les3

--- Step 5 - Handling Large Data Sets in Python/1. Numpy for Data Engineers/3_broadcasting_numpy_arrays.py
+++ Step 5 - Handling Large Data Sets in Python/1. Numpy for Data Engineers/3_broadcasting_numpy_arrays.py
@@ -15,3 +15,113 @@
 '''
 2.Broadcasting With a Single Value
 '''
+# import numpy as np
+# x = np.array([3, 2, 4, 5])
+# r = 1 / x
+# print(r)
+
+'''
+3.Broadcasting Mental Model
+'''
+# import numpy as np
+# x = np.array([
+#     [4, 2, 1, 5],
+#     [6, 7, 3, 8]
+# ])
+# y = np.array([
+#     [1],
+#     [2]
+# ])
+# z = x + y
+# print(z)
+
+'''
+4.Broadcasting Horizontally
+'''
+# import numpy as np
+# x = np.array([
+#     [4, 2, 1, 5],
+#     [6, 7, 3, 8]
+# ])
+# y = np.array([1,2,3,4])
+# z = x + y
+# print(z)
+
+'''
+5.Broadcasting Vertically
+'''
+# import numpy as np
+# x = np.array([
+#     [1], 
+#     [2],
+#     [3]
+# ])
+# y = np.array([1,2,3])
+# z = x + y
+# print(z)
+
+'''
+6.Broadcasting on Both
+'''
+# import numpy as np
+# dice1 = np.array([1,2,3,4,5,6])
+# dice2 = np.array([
+#     [1],
+#     [2],
+#     [3],
+#     [4],
+#     [5],
+#     [6]
+# ])
+# dice_sums = dice1 + dice2
+# print(dice_sums)
+
+'''
+7.Broadcasting Rules
+'''
+# import numpy as np
+# x = np.array([1, 2, 3, 4])
+# y = np.array([
+#     [1], 
+#     [2], 
+#     [3], 
+#     [4]
+# ])
+
+# shape_x = x.shape
+# print(shape_x)
+# shape_y = y.shape
+# print(shape_y)
+
+# shape_x_step1 = (1,4)
+# print(x)
+# shape_y_step1 = (4,1)
+# print(y)
+
+# shape_x_step2 = (4,4)
+# print(x)
+# shape_y_step2 = (4,4)
+# print(y)
+
+# print(x+y)
+
+# error = False
+
+'''
+8.Reshaping
+'''
+# import numpy as np
+# dice1 = np.array([1,2,3,4,5,6])
+# dice2 = dice1.reshape(6,1)
+# dice_sums = dice1 + dice2
+# print(dice_sums)
+
+'''
+9.Compatible Shapes
+'''
+import numpy as np
+cell_numbers = np.array([num for num in range(1,37)])
+numbering_by_row = cell_numbers.reshape((6,6))
+print(numbering_by_row)
+numbering_by_col = cell_numbers.reshape((6,6), order='F')
+print(numbering_by_col)
