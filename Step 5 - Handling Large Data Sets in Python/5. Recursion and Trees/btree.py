5b63e1d8609d176ca884db9dc1e10aff0843cb75
seihad <seil.hadiward@gmail.com>
2021-09-20 22:54:56 +0200

    step5 mod5 les8proj

--- Step 5 - Handling Large Data Sets in Python/5. Recursion and Trees/btree.py
+++ Step 5 - Handling Large Data Sets in Python/5. Recursion and Trees/btree.py
@@ -87,26 +87,26 @@ class BTree:
     def __len__(self):
         return self.size
     
-    def __find_node(self, current_node, key):
+    def _find_node(self, current_node, key):
         if current_node.contains_key(key):
             return current_node
         if current_node.is_leaf():
             return None
         child_index = current_node.get_insert_index(key) 
-        return self.__find_node(current_node.children[child_index], key)
+        return self._find_node(current_node.children[child_index], key)
     
     def contains(self, key):
-        node = self.__find_node(self.root, key)
+        node = self._find_node(self.root, key)
         if node is None:
             return False
         return True
     
-    def __add(self, current_node, key, value):
+    def _add(self, current_node, key, value):
         if current_node.is_leaf(): 
             current_node.insert_entry(key, value) 
         else:
             child_index = current_node.get_insert_index(key) 
-            self.__add(current_node.children[child_index], key, value)
+            self._add(current_node.children[child_index], key, value)
         if len(current_node) > self.split_threshold: 
             parent = current_node.split()  
             if current_node == self.root: 
@@ -114,11 +114,11 @@ class BTree:
                 self.height += 1
                 
     def add(self, key, value):
-        self.__add(self.root, key, value)
+        self._add(self.root, key, value)
         self.size += 1
         
     def get_value(self, key):
-        node = self.__find_node(self.root, key)
+        node = self._find_node(self.root, key)
         if node is None:
             return None
         return node.get_value(key)
