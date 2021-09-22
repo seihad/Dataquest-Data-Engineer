b9cb7de56df34a1f9d5afc6cff650c5c7373eaf8
seihad <seil.hadiward@gmail.com>
2021-09-22 22:28:28 +0200

    updates

--- Step 7 - Advanced Topics in Data/5. Spark and Map-Reduce/3_transformations_and_actions.py
+++ Step 7 - Advanced Topics in Data/5. Spark and Map-Reduce/3_transformations_and_actions.py
@@ -3,7 +3,16 @@
 # raw_hamlet.take(5)
 
 # 2.The Map Method
+split_hamlet = raw_hamlet.map(lambda line: line.split('\t'))
+split_hamlet.take(5)
+
+
+
+
 # 3.Beyond Lambda Functions
 # 4.The FlatMap Method
+hamlet_spoken_lines = split_hamlet.filter(lambda line: filter_hamlet_speaks(line))
+
+
 # 5.Filter Using a Named Function
 # 6.Actions
