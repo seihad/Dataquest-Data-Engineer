29ebe8a0d525f9f60d9973a8c686111a0754a6ee
seihad <seil.hadiward@gmail.com>
2021-09-22 22:28:16 +0200

    updates

---
+++ Step 7 - Advanced Topics in Data/5. Spark and Map-Reduce/3_transformations_and_actions.py
@@ -0,0 +1,9 @@
+# # 1.Introduction to the Data
+# raw_hamlet = sc.textFile("hamlet.txt")
+# raw_hamlet.take(5)
+
+# 2.The Map Method
+# 3.Beyond Lambda Functions
+# 4.The FlatMap Method
+# 5.Filter Using a Named Function
+# 6.Actions
